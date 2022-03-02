9b798e1b38eb7e52147aba271dbfce13062379d3
hadsss <hadyputra027@gmail.com>
2022-03-02 22:15:46 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum quod repellat tempore veniam fuga harum, repellendus sunt tenetur laboriosam dicta molestias illum rem sed dignissimos saepe accusantium amet ipsa nobis?</p>
 </body>
 </html>
